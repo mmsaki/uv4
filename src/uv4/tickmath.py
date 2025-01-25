aca7b9da174900b47897baca7c4d8c70f69991b9
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-01-25 16:32:44 -0600

    ☕️ feat: reformating and renaming

--- src/uv4/tickmath.py
+++ src/uv4/tickmath.py
@@ -2,41 +2,37 @@ from typing import Tuple
 from decimal import Decimal, getcontext
 
 getcontext().prec = 35
-D = Decimal
 
 
-def price_at_tick(tick: D) -> D:
-    return D("1.0001") ** tick
+def price_at_tick(tick: Decimal) -> Decimal:
+    return Decimal("1.0001") ** tick
 
 
-def tick_at_price(price: D) -> Decimal:
-    return D(price.log10() / D("1.0001").log10()).to_integral()
+def tick_at_price(price: Decimal) -> Decimal:
+    return Decimal(price.log10() / Decimal("1.0001").log10()).to_integral()
 
 
-def price_at_sqrt_ratio(sqrt_price: D) -> D:
-    return sqrt_price / D("2") ** D("96")
+def pricex96_at_tick(tick: Decimal) -> Decimal:
+    price = price_at_tick(tick)
+    return price_to_pricex96(price)
 
 
-def sqrt_ratio_at_price(price: D) -> D:
-    return sqrt_raito_x96(price)
-
-
-def sqrt_ratio_at_tick(tick: D) -> D:
-    return sqrt_raito_x96(price_at_tick(tick))
-
-
-def tick_at_sqrt_ratio(sqrt_price: D) -> D:
-    price = sqrt_price / (D("2") ** D("96"))
+def tick_at_pricex96(sqrt_price: Decimal) -> Decimal:
+    price = pricex96_to_price(sqrt_price)
     return tick_at_price(price)
 
 
-def sqrt_raito_x96(price: D) -> D:
-    k = D("96")
-    sqrt_price = price * (D("2") ** k)
+def price_to_pricex96(price: Decimal) -> Decimal:
+    k = Decimal("96")
+    sqrt_price = price * (Decimal("2") ** k)
     return sqrt_price
 
 
-def liquidity_y(p: D, x: D, p_a: D, p_b: D) -> D:
+def pricex96_to_price(price_x96: Decimal) -> Decimal:
+    return price_x96 / Decimal("2") ** Decimal("96")
+
+
+def liquidity_y(p: Decimal, x: Decimal, p_a: Decimal, p_b: Decimal) -> Decimal:
     """
     ETH/USDC
     p: <decimal> current price of token0 e.g. 2000 USDC
@@ -50,9 +46,9 @@ def liquidity_y(p: D, x: D, p_a: D, p_b: D) -> D:
     return y
 
 
-def percentage_to_tick_bounds(price: D, rate: D) -> Tuple[D, D]:
+def percentage_to_tick_bounds(price: Decimal, rate: Decimal) -> Tuple[Decimal, Decimal]:
     mid = tick_at_price(price)
-    assert rate >= D("0.01")
-    low = mid - rate * D("100")
-    high = mid + rate * D("100")
+    assert rate >= Decimal("0.01")
+    low = mid - rate * Decimal("100")
+    high = mid + rate * Decimal("100")
     return low, high
