becbfc8525fa5d9f3ccd9c7e11ca9ff8f8023767
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-02-21 08:57:25 -0600

    feat: 🪴 add new liquidty funcs

--- src/uv4/__init__.py
+++ src/uv4/__init__.py
@@ -5,7 +5,9 @@ from .hook import Hook
 from .Q6496 import Q6496
 from .tickmath import TickMath
 from .liquidity import (
-    liquidity_y,
+    liquidity_y_from_prices,
+    liquidity_y_from_ticks,
+    liquidity_y_from_sqrt_prices,
     percentage_slippage_to_tick_bounds,
 )
 from .utils import (
@@ -19,6 +21,10 @@ __all__ = [
     "TickMath",
     "Q6496",
     "integer_to_binary_string",
+    "liquidity_y_from_prices",
+    "liquidity_y_from_ticks",
+    "liquidity_y_from_sqrt_prices",
+    "percentage_slippage_to_tick_bounds",
 ]
 
 
@@ -27,27 +33,42 @@ def main() -> None:
     t = TickMath()
     parser = optparse.OptionParser()
     parser.add_option(
-        "--get_sqrt_price_at_tick",
+        "--get_sqrt_price_x96_at_tick",
         "-s",
+        type="string",
+        help="Get square root ratin at tick",
+    )
+    parser.add_option(
+        "--get_tick_at_sqrt_price_x96",
+        "-t",
         type="string",
         help="Get square root ratin at tick",
     )
     parser.add_option("--price_at_tick", "-p", type=int, help="Get price at tick")
     parser.add_option(
-        "--liquidity",
+        "--liquidity_from_prices",
         "-l",
         type="string",
         action="callback",
         callback=get_values,
         help="""Get liquididity y between price range
-            <price>,<amount_in>,<min_price>,<max_price>
-            price p, amount x, liquidity range [p_a, p_b]
+            <price>,<amount0_in>,<min_price>,<max_price>
+            price p, amount0 x, liquidity range [p_a, p_b]
+        """,
+    )
+    parser.add_option(
+        "--liquidity_from_ticks",
+        type="string",
+        action="callback",
+        callback=get_values,
+        help="""Get liquididity y between price range
+            <current_tick>,<amount0_in>,<tick_lower>,<tick_upper>
+            tick p, amount0 x, liquidity range [p_a, p_b]
         """,
     )
 
     parser.add_option(
         "--tick_bounds",
-        "-t",
         type="string",
         action="callback",
         callback=get_values,
@@ -59,13 +80,21 @@ def main() -> None:
     opts, args = parser.parse_args()
     if opts:
         d = opts.__dict__
-        sqrt = "get_sqrt_price_at_tick"
-        if d[sqrt]:
-            tick = d[sqrt]
+        sqrt_label = "get_sqrt_price_x96_at_tick"
+        if d[sqrt_label]:
+            tick = d[sqrt_label]
             if tick is not None:
                 t.tick = int(tick)
                 sqrtx96 = t.to_sqrt_price_x96()
                 print(f"0x{encode(['uint160'], [sqrtx96]).hex()}")
+
+        tick_label = "get_tick_at_sqrt_price_x96"
+        if d[tick_label]:
+            sqrtx96 = d[tick_label]
+            if sqrtx96 is not None:
+                sqrt_x96 = int(sqrtx96)
+                tick = t.from_sqrt_pricex96(sqrt_x96)
+                print(f"0x{encode(['int24'], [tick]).hex()}")
 
         price = "price_at_tick"
         if d[price]:
@@ -75,11 +104,17 @@ def main() -> None:
                 price = t.to_price()
                 print(f"0x{encode(['uin160'], [price]).hex()}")
 
-        liquidity = "liquidity"
+        liquidity = "liquidity_from_prices"
         if d[liquidity]:
             values = [Decimal(i) for i in d[liquidity]]
             if values is not None:
-                print(f"{liquidity}({values}) = {liquidity_y(*values)}")
+                print(f"{liquidity_y_from_prices(*values)}")
+
+        liquidity = "liquidity_from_ticks"
+        if d[liquidity]:
+            values = [Decimal(i) for i in d[liquidity]]
+            if values is not None:
+                print(f"{liquidity_y_from_ticks(*values)}")
 
         ticks = "tick_bounds"
         if d[ticks]:
