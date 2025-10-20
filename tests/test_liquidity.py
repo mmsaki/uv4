5f00a89fdcdd6fa2b18ebf8a46e09eaa94881f12
Meek Msaki <meek10x@gmail.com>
2025-10-19 20:51:41 -0500

    use fixture for tickmath

--- tests/test_liquidity.py
+++ tests/test_liquidity.py
@@ -11,6 +11,11 @@ def liq():
     return Liquidity()
 
 
+@pytest.fixture
+def tm():
+    return TickMath()
+
+
 @pytest.mark.parametrize(
     ("liquidity", "tick_lower", "tick_upper", "sqrt_price"),
     # https://app.uniswap.org/positions/v3/ethereum/37
@@ -18,8 +23,8 @@ def liq():
         (10860507277202, 192180, 193380, 1906627091097897970122208862883908),
     ],
 )
-def test_position37v3(liq, liquidity, tick_lower, tick_upper, sqrt_price):
-    tick = TickMath().from_sqrt_pricex96(sqrt_price)
+def test_position37v3(liq, tm, liquidity, tick_lower, tick_upper, sqrt_price):
+    tick = tm.from_sqrt_pricex96(sqrt_price)
     # position is not in range
     is_inrange = liq.is_position_in_range(tick_lower, tick_upper, tick)
     assert not is_inrange
@@ -42,10 +47,10 @@ def test_position37v3(liq, liquidity, tick_lower, tick_upper, sqrt_price):
         (555103547015, -887270, 887270, 1260437594239115943190250841240651),
     ],
 )
-def test_position1v4(liq, liquidity, tick_lower, tick_upper, sqrt_price):
-    tick = TickMath().from_sqrt_pricex96(sqrt_price)
+def test_position1v4(liq, tm, liquidity, tick_lower, tick_upper, sqrt_price):
+    tick = tm.from_sqrt_pricex96(sqrt_price)
     # position is not in range
-    is_inrange = Liquidity().is_position_in_range(tick_lower, tick_upper, tick)
+    is_inrange = liq.is_position_in_range(tick_lower, tick_upper, tick)
     assert is_inrange
     p = TickMath().to_price(tick)
     p_u = TickMath().to_price(tick_upper)
