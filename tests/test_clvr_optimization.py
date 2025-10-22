b8f4006115f08f475e4dd9ed6558e343c3903bb1
Meek Msaki <meek10x@gmail.com>
2025-10-21 20:25:53 -0500

    refactor fn vars

--- tests/test_clvr_optimization.py
+++ tests/test_clvr_optimization.py
@@ -7,7 +7,7 @@ def k():
 
 
 @pytest.mark.parametrize(
-    ("current_tick", "ticks"),
+    ("initial_tick", "ticks"),
     [
         (5, (7, 6, 5, 4, 3, 1)),
         (5, (1, 3, 4, 5, 6, 7)),
@@ -16,20 +16,20 @@ def k():
         (5, (6, 5, 4, -1, -3, 7)),
     ],
 )
-def test_clvr(k, current_tick, ticks):
+def test_clvr(k, initial_tick, ticks):
     accumulator = 0
     assert k < len(ticks)
     for i in range(k, len(ticks)):
-        a = 0
-        b = ticks[i]
+        previous_tick = 0
+        current_tick = ticks[i]
         if i == k:
-            a = current_tick
+            previous_tick = initial_tick
         else:
-            a = ticks[i - k]
-        diff = b - a
+            previous_tick = ticks[i - k]
+        diff = current_tick - previous_tick
         diff_sqrd = diff**2
         accumulator += diff_sqrd
 
-    print("\ncumm", accumulator)
+    print("\naccumulator", accumulator)
     volatility = accumulator / (len(ticks) - k)
     print("volatility", volatility)
